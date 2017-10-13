0681f3bf049052332c0f77299ede4f3d1466000e
chongyangzhong <chongyang741586437@gmail.com>
2017-10-13 11:57:40 +1100

    Add files via upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,10 @@
 class Post < ApplicationRecord
-	belongs_to :user, optional: true
-	default_scope -> { order(created_at: :desc) }
-	validates :user_id, presence: true
-	validates :body, presence: true
-	
+	has_attached_file :image, styles: { large: "600*600", medium: "300*300", thumb: "150*150#"}
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+	validates :title, presence: true,
+						length: {minimum: 5}
 
-	has_many :comments
-    validates :title, presence: true, length: {minimum: 4}
-    def self.search(search)
+	def self.search(search)
     	
     	where("title LIKE ? OR body LIKE ?","%#{search}%", "%#{search}%")
     	
