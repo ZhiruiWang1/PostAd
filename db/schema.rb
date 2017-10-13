fbaf45f7ee2e0ac05d943d619705bf9ee80f0d80
Zhirui Wang <DanielWangDirk1013@outlook.com>
2017-10-13 20:39:53 +1100

    changed database to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012091053) do
+ActiveRecord::Schema.define(version: 20171013050611) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(version: 20171012091053) do
     t.datetime "updated_at", null: false
     t.integer "lock_version", default: 0, null: false
     t.string "category"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
