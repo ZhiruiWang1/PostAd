c811f7fe3cd5587b447ae71a141e419d71cd45ce
chongyangzhong <chongyang741586437@gmail.com>
2017-10-13 16:13:21 +1100

    Add files via upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-	#has_attached_file :image, styles: { large: "600*600", medium: "300*300", thumb: "150*150#"}
-	#validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+	has_attached_file :image, styles: { large: "600*600", medium: "300*300", thumb: "150*150#"}
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	#validates :title, presence: true,
 						#length: {minimum: 5}
 	belongs_to :user, optional: true
