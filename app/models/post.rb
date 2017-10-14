1a1d9ebc97f64cebfefe07d2f93680e0208255c3
rozar17 <30593099+rozar17@users.noreply.github.com>
2017-10-14 20:33:32 +1100

    Update post.rb
    added in validation comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,22 +1,27 @@
 class Post < ApplicationRecord
-	has_attached_file :image, styles: { large: "600*600", medium: "300*300", thumb: "150*150#"}
-	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-	#validates :title, presence: true,
+
+  has_attached_file :image, styles: { large: "600*600", medium: "300*300", thumb: "150*150#"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  #validates :title, presence: true,
 						#length: {minimum: 5}
-	belongs_to :user, optional: true
-	default_scope -> { order(created_at: :desc) }
-	validates :user_id, presence: true
-	validates :body, presence: true
+  belongs_to :user, optional: true
+  default_scope -> { order(created_at: :desc) }
+	
+  #validation rules
+  validates :user_id, presence: true
+  validates :body, presence: true
 	
 
-	has_many :comments
-	validates :title, presence: true, length: {minimum: 4}					
+  has_many :comments
+	
+  #validation rules
+  validates :title, presence: true, length: {minimum: 4}					
 
 	
-	#Method for Searching Post records - database lookup
-	def self.search(search)
-    	   where("title LIKE ? OR body LIKE ?","%#{search}%", "%#{search}%")
-    	   #where("body LIKE ?","%#{search}%")
-       end
+  #Method for Searching Post records - database lookup
+  def self.search(search)
+    where("title LIKE ? OR body LIKE ?","%#{search}%", "%#{search}%")
+    #where("body LIKE ?","%#{search}%")
+  end
 
 end
