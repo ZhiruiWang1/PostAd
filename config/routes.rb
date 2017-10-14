4403ffee044823bec2baef3a580888f443d423bd
rozar17 <30593099+rozar17@users.noreply.github.com>
2017-10-14 19:02:29 +1100

    Update routes.rb
    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Main page that opens is root
+  root 'posts#index', as: 'home' # as 'home' is needed as its defined in \app\views\layouts\application.html.erb
 
-root 'posts#index', as: 'home'
+  get'about' => 'pages#about', as: 'about'
 
-get'about' => 'pages#about', as: 'about'
-
-resources :posts do
-resources :comments
-
-end
-
-
+  # Resources creates seven different routes in this application, all mapping to the Posts controller
+  # They are the index, new, create, show, edit, update and destroy actions
+  resources :posts do
+    # This resources maps to Comments controller
+    resources :comments
+  end
 
 end
