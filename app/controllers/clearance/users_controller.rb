3d7d731099d712f4eaea00b62904a2c9beb5c57e
rozar17 <30593099+rozar17@users.noreply.github.com>
2017-10-15 15:18:48 +1100

    Update users_controller.rb
    added in comments

--- app/controllers/clearance/users_controller.rb
+++ app/controllers/clearance/users_controller.rb
@@ -1,4 +1,6 @@
 class Clearance::UsersController < Clearance::BaseController
+  
+  # Stop access to pages unless logged in - using Clearance helper
   if respond_to?(:before_action)
     before_action :redirect_signed_in_users, only: [:create, :new]
     skip_before_action :require_login, only: [:create, :new], raise: false
@@ -8,12 +10,15 @@ class Clearance::UsersController < Clearance::BaseController
     skip_before_filter :require_login, only: [:create, :new], raise: false
     skip_before_filter :authorize, only: [:create, :new], raise: false
   end
+  
+  
 
   def new
     @user = user_from_params
     render template: "users/new"
   end
-
+  
+  
   def create
     @user = user_from_params
 
@@ -24,7 +29,10 @@ class Clearance::UsersController < Clearance::BaseController
       render template: "users/new"
     end
   end
-
+  
+  
+  
+  # Private methods for this class
   private
 
   def avoid_sign_in
@@ -35,16 +43,19 @@ class Clearance::UsersController < Clearance::BaseController
     redirect_signed_in_users
   end
 
+  
   def redirect_signed_in_users
     if signed_in?
       redirect_to Clearance.configuration.redirect_url
     end
   end
 
+  
   def url_after_create
     Clearance.configuration.redirect_url
   end
 
+  
   def user_from_params
     email = user_params.delete(:email)
     password = user_params.delete(:password)
@@ -54,6 +65,7 @@ class Clearance::UsersController < Clearance::BaseController
       user.password = password
     end
   end
+  
 
   def user_params
     params[Clearance.configuration.user_parameter] || Hash.new
