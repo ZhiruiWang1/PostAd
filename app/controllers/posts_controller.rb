02a15a76d9ddf18b20a537fe40abf063dad055ac
rozar17 <30593099+rozar17@users.noreply.github.com>
2017-10-15 15:29:37 +1100

    Update posts_controller.rb
    added in comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,59 +1,70 @@
 class PostsController < ApplicationController
-	before_action :require_login, only: [:new, :edit, :show, :destroy]
-	def index
-		@posts = Post.all
+	
+  # Stop access to pages unless logged in - using Clearance helper
+  before_action :require_login, only: [:new, :edit, :show, :destroy]
+
+
+  def index
+    @posts = Post.all
+
+    #Search Post records based on sql in model
+    if params[:search]
+      @posts = Post.search(params[:search]).order("created_at DESC")
+    else
+      @posts = Post.all.order('created_at DESC')
+    end
+  end
+
+
+  def show
+    @post =Post.find(params[:id])
+  end
+
+	
+  def new
+    @post = Post.new
+  end
+
+
+  def edit
+    @post =Post.find(params[:id])
 		
-		#Search Post records based on sql in model
-		if params[:search]
-			@posts = Post.search(params[:search]).order("created_at DESC")
-		else
-			@posts = Post.all.order('created_at DESC')
-		end
-	end
+  end
 
-	def show
-		@post =Post.find(params[:id])
-	end
 
-	def new
-		@post = Post.new
-	end
+  def update
+    @post =Post.find(params[:id])
+    if(@post.update(post_params)
+      redirect_to @post)
+    else
+      render 'edit'
+    end
+  end
 
-	def edit
-		@post =Post.find(params[:id])
-		
-	end
 
-	def update
-		@post =Post.find(params[:id])
+  def create
+    #render plain: params[:post].inspect
+    @post = Post.new(post_params)
+    @post.user = current_user
 
-		if(@post.update(post_params)
-			redirect_to @post)
-	else
-		render 'edit'
-	end
-	end
+    if (@post.save)
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
 
-	def create
-		#render plain: params[:post].inspect
-		@post = Post.new(post_params)
-		@post.user = current_user
 
-		if (@post.save)
-		redirect_to @post
-	else
-			render 'new'
-	end
-	end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
 
-	def destroy
-		@post = Post.find(params[:id])
-		@post.destroy
 
-		redirect_to posts_path
-	end
-
-	private def post_params
-		params.require(:post).permit(:title, :body, :image)
-	end
+	
+  # Private methods for this class
+  private def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
 end
